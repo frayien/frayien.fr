91e9284677de472324a93092607c0c321c7f21a3
Adrien <garbani.adrien@yahoo.fr>
2021-07-18 02:27:05 +0200

    site

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,25 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+        <v-card>
+          <v-card-title>About</v-card-title>
+          <v-card-text> 
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex quisquam aperiam laborum nam nostrum, corrupti minima nemo tempora dolore, ullam possimus libero ipsum. Veniam, at iusto consequatur dolor expedita voluptates.</p>
+          </v-card-text>
+          <v-card-actions>
+            <v-alert type="success">
+              Salut
+            </v-alert>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+
+<script>
+export default {
+  name: 'About',
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,25 @@
 <template>
-  <div class="home">
-    <img
-      alt="Vue logo"
-      src="../assets/logo.png"
-    >
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+        <v-card>
+          <v-card-title>Hello World !</v-card-title>
+          <v-card-text>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus ratione laboriosam hic sit, vitae, rem cumque odit eius quae obcaecati ipsam possimus aliquid modi facere numquam deleniti natus, asperiores velit!</p>
+          </v-card-text>
+          <v-card-actions>
+            <v-alert type="success">
+              Salut
+            </v-alert>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld,
-  },
 };
 </script>
